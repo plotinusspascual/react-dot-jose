888674ec90dd9ae73a9072448701a2a5aa30df3f
plotinusspascual <plotinusspascual@gmail.com>
2022-03-04 10:10:05 -0800

    changed styling in home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,54 +13,29 @@ function Home(){
       backgroundRepeat: 'no-repeat',
       color: 'white',
     },
-    about: {
-      width: '80%',
-      padding: '20px',
-      color: 'white',
-    },
     picture: {
       width: '300px'
     },
-    paragraph: {
-      fontSize: '25px'
-    },
-    background: {
-      backgroundColor: '#706B62',
-      paddingBottom: '30px'
+    content: {
+      paddingTop: '250px'
     }
-    
   }
+  
   return(
-  <section id="home" style={styles.background}>
+  <section id="home">
     <header class="jumbotron" style={styles.jumbotron}>
-      <div class="container h-100">
-        <div class="row h-100 align-items-center">
+      <div class="container" style={styles.content}>
+        <div class="row align-items-start">
           <div class="col-12 text-center">
             <h1 class="fw-light">console.log('Hi, Im Jose');</h1>
+          </div>
+          <div class="col-12">
+            <img src="https://i.imgur.com/j9AIHfL.jpg " class="rounded-circle mx-auto d-block" style={styles.picture}></img>
           </div>
         </div>
       </div>
     </header>
-    <div class="container border border-light border-2 rounded" style={styles.about}>
-      <div class="row align-items-start">
-      <h1>Get To Know Me</h1>
-        <div class="col" style={styles.paragraph}> 
-          <p> 
-            I am recent Computer Science graduate, who's studies are focused
-            on web design and development. I'm seeking to apply my skills on
-            real-world projects and deepen my understanding of software and
-            web develeopment. Currently taking a full stack bootcamp from University
-            of California Berkeley's extension program with an emphasis on front
-            end. 
-          </p>
-        </div>
-        <div class="col">
-          <img src="https://i.imgur.com/j9AIHfL.jpg " class="rounded-circle mx-auto d-block" style={styles.picture}></img>
-        </div>
-      </div>   
-    </div>  
   </section>
-
   )
 }
 
