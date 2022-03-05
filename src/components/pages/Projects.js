2892818bfc551c7d066fafdf03784e102e0aa28b
plotinusspascual <plotinusspascual@gmail.com>
2022-03-04 23:07:12 -0800

    styled projects component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -14,6 +14,7 @@ export default function Projects(){
     },
     projects: {
       width: '18rem',
+      height: '500px',
       padding: '10px',
       margin: 'auto'
     },
@@ -25,7 +26,11 @@ export default function Projects(){
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
       color: 'black',
-    }
+    },
+    image: {
+      width: '100%',
+      height: '250px'
+    }  
   }
   return (
     <section id="projects" style={styles.jumbotron}>
@@ -33,38 +38,38 @@ export default function Projects(){
         <div class="row align-items-start">
         <h1>My Projects</h1>
         <div class="card" style={styles.projects}>
-          <img src="https://i.imgur.com/HDvbG1d.png" class="card-img-top"></img>
+          <img src="https://i.imgur.com/HDvbG1d.png" class="card-img-top" style={styles.image}></img>
           <div class="card-body">
             <h2>FeedMe</h2>
             <p class="card-text">A restaurant randomizer that gets users location and allows for filtered searches</p>
-            <a href="#" class="btn btn-primary">Check it out!</a>
+            <a href="https://github.com/PentaZoned/feed-me" class="btn btn-dark" target="_blank">Check it out!</a>
           </div>
         </div>          
 
         <div class="card" style={styles.projects}>
-          <img src="https://i.imgur.com/HDvbG1d.png" class="card-img-top"></img>
+          <img src="https://i.imgur.com/44qmSC1.jpg" class="card-img-top" style={styles.image}></img>
           <div class="card-body">
-            <h2>FeedMe</h2>
-            <p class="card-text">A restaurant randomizer that gets users location and allows for filtered searches</p>
-            <a href="#" class="btn btn-primary">Check it out!</a>
+            <h2>DontFundMe</h2>
+            <p class="card-text">Crowdfunding application where users can tell their stories to get funding or support someone else's story</p>
+            <a href="https://github.com/PentaZoned/DontFundMe" class="btn btn-dark" target="_blank">Check it out!</a>
           </div>
         </div>          
 
         <div class="card" style={styles.projects}>
-          <img src="https://i.imgur.com/HDvbG1d.png" class="card-img-top"></img>
+          <img src="https://i.imgur.com/67c6FBc.jpg" class="card-img-top" style={styles.image}></img>
           <div class="card-body">
-            <h2>FeedMe</h2>
-            <p class="card-text">A restaurant randomizer that gets users location and allows for filtered searches</p>
-            <a href="#" class="btn btn-primary">Check it out!</a>
+            <h2>Social Network API</h2>
+            <p class="card-text">A Backend API for social networking using MongoDB</p>
+            <a href="https://github.com/plotinusspascual/social-network-api" class="btn btn-dark" target="_blank">Check it out!</a>
           </div>
         </div>         
 
         <div class="card" style={styles.projects}>
-          <img src="https://i.imgur.com/HDvbG1d.png" class="card-img-top"></img>
+          <img src="https://i.imgur.com/Vqjyasf.png" class="card-img-top" style={styles.image}></img>
           <div class="card-body">
-            <h2>FeedMe</h2>
-            <p class="card-text">A restaurant randomizer that gets users location and allows for filtered searches</p>
-            <a href="#" class="btn btn-primary">Check it out!</a>
+            <h2>ReadMe Generator</h2>
+            <p class="card-text">An easy to use application that takes users input and generates a ReadMe</p>
+            <a href="https://github.com/plotinusspascual/readme-generator" class="btn btn-dark" target="_blank">Check it out!</a>
           </div>
         </div>
         </div>
