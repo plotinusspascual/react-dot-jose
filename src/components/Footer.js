680b63e172067b3878f74bfd6e2c51aea96758e1
plotinusspascual <plotinusspascual@gmail.com>
2022-03-04 21:50:37 -0800

    styled contact components and footer components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BsGithub, BsInstagram, BsTwitter, BsLinkedin  } from 'react-icons/bs';
+import { SiGmail } from 'react-icons/si';
 
 function Footer(){
 
@@ -12,29 +14,31 @@ function Footer(){
     <div className="footer bg-dark text-center text-white" style={styles.footer}>
       <div class="container p-4 pb-0">
     <section class="mb-4">
-      <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
-        ><i class="fab fa-facebook-f"></i
-      ></a>
+      
+      <a class="btn btn-outline-light btn-floating m-1" 
+        href="https://www.instagram.com/plotinussp/" 
+        role="button" target="_blank">
+        <i class="fab fa-instagram-f"><BsInstagram /></i>
+      </a>
 
-      <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
-        ><i class="fab fa-twitter"></i
-      ></a>
+      <a class="btn btn-outline-light btn-floating m-1" 
+        href="mailto:plotinusspascual@gmail.com" 
+        role="button" target="_blank">
+        <i class="fab fa-twitter"><SiGmail /></i>
+      </a>
+      
+      <a class="btn btn-outline-light btn-floating m-1" 
+        href="https://www.linkedin.com/in/jose-plotinuss-pascual/" 
+        role="button" target="_blank">
+        <i class="fab fa-linkedin-in"><BsLinkedin /></i>
+      </a>
 
-      <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
-        ><i class="fab fa-google"></i
-      ></a>
+      <a class="btn btn-outline-light btn-floating m-1" 
+        href="https://github.com/plotinusspascual/" 
+        role="button" target="_blank">
+        <i class="fab fa-github"><BsGithub /></i>
+      </a>
 
-      <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
-        ><i class="fab fa-instagram"></i
-      ></a>
-
-      <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
-        ><i class="fab fa-linkedin-in"></i
-      ></a>
-
-      <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
-        ><i class="fab fa-github"></i
-      ></a>
     </section>
   </div>
 
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,19 +1,32 @@
 import React from "react";
+import { IconName } from "react-icons/ai";
 
 export default function Contact(){
+  const styles = {
+    form: {
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: '20px'
+    },
+    width: {
+      width: '500px'
+    }
+  };
+
   return (
-    <section id="about">
-      <div className="container">
-        <h1>About Me</h1>
-        <p>
-          I am recent Computer Science graduate, who's studies are focused
-          on web design and development. I'm seeking to apply my skills on
-          real-world projects and deepen my understanding of software and
-          web develeopment. Currently taking a full stack bootcamp from University
-          of California Berkeley's extension program with an emphasis on front
-          end. 
-        </p>  
-      </div>
+    <section style={styles.form}>
+    <div class="mb-3" style={styles.width}>
+      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+    </div>
+    <div class="mb-3" style={styles.width}>
+      <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <button type="button" class="btn btn-dark" style={styles.width}>Send</button>
     </section>
-  );
+  )
 }
