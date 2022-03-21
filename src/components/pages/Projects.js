a9cd8ea6d634631680ce6221138b61534bee50cc
plotinusspascual <plotinusspascual@gmail.com>
2022-03-21 13:55:18 -0700

    changed styling of project component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -14,7 +14,7 @@ export default function Projects(){
     },
     projects: {
       width: '18rem',
-      height: '500px',
+      height: '550px',
       padding: '10px',
       margin: 'auto',
       color: 'black',
@@ -23,6 +23,12 @@ export default function Projects(){
     image: {
       width: '100%',
       height: '250px'
+    },
+    button: {
+      position: 'absolute',
+      bottom: '0',
+      left: '0',
+      right: '0'
     }  
   }
   return (
@@ -35,7 +41,7 @@ export default function Projects(){
           <div class="card-body">
             <h2>FeedMe</h2>
             <p class="card-text">A restaurant randomizer that gets users location and allows for filtered searches</p>
-            <a href="https://github.com/PentaZoned/feed-me" class="btn btn-dark" target="_blank">Check it out!</a>
+            <a href="https://plotinusspascual.github.io/feed-me/" class="btn btn-dark" target="_blank" style={styles.button}>Check it out!</a>
           </div>
         </div>          
 
@@ -44,7 +50,7 @@ export default function Projects(){
           <div class="card-body">
             <h2>DontFundMe</h2>
             <p class="card-text">Crowdfunding application where users can tell their stories to get funding or support someone else's story</p>
-            <a href="https://github.com/PentaZoned/DontFundMe" class="btn btn-dark" target="_blank">Check it out!</a>
+            <a href="https://github.com/PentaZoned/DontFundMe" class="btn btn-dark" target="_blank" style={styles.button}>Check it out!</a>
           </div>
         </div>          
 
@@ -53,16 +59,16 @@ export default function Projects(){
           <div class="card-body">
             <h2>Social Network API</h2>
             <p class="card-text">A Backend API for social networking using MongoDB</p>
-            <a href="https://github.com/plotinusspascual/social-network-api" class="btn btn-dark" target="_blank">Check it out!</a>
+            <a href="https://github.com/plotinusspascual/social-network-api" class="btn btn-dark" target="_blank" style={styles.button}>Check it out!</a>
           </div>
         </div>         
 
         <div class="card" style={styles.projects}>
-          <img src="https://i.imgur.com/Vqjyasf.png" class="card-img-top" style={styles.image}></img>
+          <img src="https://i.imgur.com/xgoiL0z.png" class="card-img-top" style={styles.image}></img>
           <div class="card-body">
-            <h2>ReadMe Generator</h2>
-            <p class="card-text">An easy to use application that takes users input and generates a ReadMe</p>
-            <a href="https://github.com/plotinusspascual/readme-generator" class="btn btn-dark" target="_blank">Check it out!</a>
+            <h2>GITMATCH</h2>
+            <p class="card-text">Matching APP for developers to GitMatch'ed with other developers to collaborate</p>
+            <a href="https://morning-sea-85016.herokuapp.com/" class="btn btn-dark" target="_blank" style={styles.button}>Check it out!</a>
           </div>
         </div>
         </div>
